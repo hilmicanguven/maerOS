2ddac0846f87b403109bbe0dc2170c3c69587354
hilmi <hilmicanguven@gmail.com>
2024-05-23 23:52:31 +0300

    read from stack part is added

--- src/task/process.c
+++ src/task/process.c
@@ -427,7 +427,6 @@ int process_map_memory(struct process* process)
 
     res = process_map_binary(process);
 
-    return res;
 //     switch(process->filetype)
 //     {
 //         case PROCESS_FILETYPE_ELF:
@@ -442,15 +441,15 @@ int process_map_memory(struct process* process)
 //             panic("process_map_memory: Invalid filetype\n");
 //     }
 
-//     if (res < 0)
-//     {
-//         goto out;
-//     }
+     if (res < 0)
+     {
+         goto out;
+     }
 
-//     // Finally map the stack
-//     paging_map_to(process->task->page_directory, (void*)MAEROS_PROGRAM_VIRTUAL_STACK_ADDRESS_END, process->stack, paging_align_address(process->stack+MAEROS_USER_PROGRAM_STACK_SIZE), PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL | PAGING_IS_WRITEABLE);
-// out:
-//     return res;
+     // Finally map the stack
+     paging_map_to(process->task->page_directory, (void*)MAEROS_PROGRAM_VIRTUAL_STACK_ADDRESS_END, process->stack, paging_align_address(process->stack+MAEROS_USER_PROGRAM_STACK_SIZE), PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL | PAGING_IS_WRITEABLE);
+ out:
+     return res;
 }
 
 /** @brief pass through process array and find empty slot */
